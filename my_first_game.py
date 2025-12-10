fc77c7579020ac81569f71d846742e11de7b705b
thomas <thomas@gupi.fr>
2025-12-09 18:20:23 -0800

    wrapps from the adges and added backround placeholder

--- my_first_game.py
+++ my_first_game.py
@@ -19,30 +19,48 @@ class Game:
         """This runs every frame to update the game"""
         # Check if player pressed arrow keys and move character
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.x = self.x - 2  # Move left
+            self.x = self.x - 2  #move lef
+            if self.x < 0:
+                self.x = 160  # Went off left, appear on right
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.x = self.x + 2  # Move right
+            if self.x > 160:
+                self.x = 0  # Went off right, appear on left        # Wrap around screen edges
+
         if pyxel.btn(pyxel.KEY_UP):
-            self.y = self.y - 2  # Move up
+            self.y = self.y - 2  # Move  # Went off left, appear on right
+            if self.y < 0:
+                self.y = 120  # Went off top, appear on bottom   
         if pyxel.btn(pyxel.KEY_DOWN):
             self.y = self.y + 2  # Move down
+            if self.y > 120:
+                self.y = 0  # Went off bottom, appear on top
                     # Check if mouse button is pressed
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             self.color = self.color + 1  # Add 1 to color
         if self.color==16 :
             self.color = 0
     
+
     
     def draw(self):
         """This runs every frame to draw everything on screen"""
         # Clear the screen with color 0 (black)
         pyxel.cls(0)
+        pyxel.circ(50, 50, 3, 3)    # A green circle
+        pyxel.circ(100, 80, 3, 3)   # Another green circle
+        pyxel.circ(150, 30, 3, 11)
+                # Draw a road
+        pyxel.rect(0, 90, 160, 10, 13)  # A gray road
+
         
+
         # Draw a circle at position (x, y) with radius 8 and color 11 (light blue)
         pyxel.circ(self.x, self.y, 5, self.color)
         
         # Draw some text at the top
         pyxel.text(10, 10, "Herro!", 7)
+
 
 # Start the game!
 Game()
